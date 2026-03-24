3a84ff5109646fb76cb499bb05fe8cc14c113de3
RagnhildSG <Ragnhild2002@hotmail.com>
2026-03-24 19:32:37 +0100

    Del 2

---
+++ src/main/java/org/example/AnsattDAO.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class AnsattDAO {
+
+    private EntityManager em;
+
+    public AnsattDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public Ansatt finnAnsattMedId(int id) {
+        return em.find(Ansatt.class, id);
+    }
+
+    public Ansatt finnAnsattMedBrukernavn(String brukernavn) {
+        TypedQuery<Ansatt> query = em.createQuery(
+                "SELECT a FROM Ansatt a WHERE a.brukernavn = :bn", Ansatt.class);
+        query.setParameter("bn", brukernavn);
+        return query.getSingleResult();
+    }
+
+    public List<Ansatt> finnAlleAnsatte() {
+        TypedQuery<Ansatt> query = em.createQuery(
+                "SELECT a FROM Ansatt a", Ansatt.class);
+        return query.getResultList();
+    }
+
+    public void oppdaterAnsatt(int id, String nyStilling, Double nyLonn) {
+        Ansatt a = em.find(Ansatt.class, id);
+        if (a != null) {
+            em.getTransaction().begin();
+            if (nyStilling != null) a.setStilling(nyStilling);
+            if (nyLonn != null) a.setMaanedslonn(nyLonn);
+            em.getTransaction().commit();
+        }
+    }
+
+    public void lagreAnsatt(Ansatt ansatt) {
+        em.getTransaction().begin();
+        em.persist(ansatt);
+        em.getTransaction().commit();
+    }
+}
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,35 +7,122 @@ import jakarta.persistence.Persistence;
 import java.util.Scanner;
 
 public class Main {
+
     private static EntityManagerFactory emf;
+
     static {
         emf = Persistence.createEntityManagerFactory("grethes-persistence-unit");
     }
 
-    /* ------------------------------------------------------------------- */
-
     public static void main(String[] args) {
+
+        EntityManager em = emf.createEntityManager();
+        AnsattDAO dao = new AnsattDAO(em);
 
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Skrive et ansatt id:");
+        boolean ferdig = false;
 
-        int temp = in.nextInt();
+        while (!ferdig) {
 
-        Ansatt p = finnPersonMedID(temp);
-        System.out.println(p);
-        in.close();
-    }
+            System.out.println("\n--- MENY ---");
+            System.out.println("1: Finn ansatt (ID)");
+            System.out.println("2: Finn ansatt (brukernavn)");
+            System.out.println("3: List alle ansatte");
+            System.out.println("4: Oppdater ansatt");
+            System.out.println("5: Ny ansatt");
+            System.out.println("0: Avslutt");
 
-    private static Ansatt finnPersonMedID(int ansattid) {
+            System.out.print("Velg: ");
+            int valg = in.nextInt();
 
-        System.out.println("Kobler til database...");
-        EntityManager em = emf.createEntityManager();
+            switch (valg) {
 
-        try {
-            return em.find(Ansatt.class, ansattid);
-        } finally {
-            em.close();
+                case 1:
+                    System.out.print("Skriv ansatt-id: ");
+                    int id = in.nextInt();
+
+                    Ansatt a1 = dao.finnAnsattMedId(id);
+
+                    if (a1 != null) {
+                        System.out.println(a1);
+                    } else {
+                        System.out.println("Fant ikke ansatt.");
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("Brukernavn: ");
+                    String bn = in.next();
+
+                    Ansatt a2 = dao.finnAnsattMedBrukernavn(bn);
+
+                    if (a2 != null) {
+                        System.out.println(a2);
+                    } else {
+                        System.out.println("Fant ikke ansatt.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("\n--- Alle ansatte ---");
+                    dao.finnAlleAnsatte().forEach(System.out::println);
+                    break;
+
+                case 4:
+                    System.out.print("Ansatt-id: ");
+                    int idOppdatert = in.nextInt();
+
+                    System.out.print("Ny stilling (skriv - for å hoppe over): ");
+                    String stilling = in.next();
+
+                    if (stilling.equals("-")) {
+                        stilling = null;
+                    }
+
+                    System.out.print("Ny lønn (skriv -1 for å hoppe over): ");
+                    double lonnInput = in.nextDouble();
+                    Double lonn = (lonnInput == -1) ? null : lonnInput;
+
+                    dao.oppdaterAnsatt(idOppdatert, stilling, lonn);
+                    System.out.println("Oppdatert.");
+                    break;
+
+                case 5:
+                    Ansatt ny = new Ansatt();
+
+                    // Enkelt – du kan hardkode eller spørre bruker
+                    System.out.print("Fornavn: ");
+                    ny.setFornavn(in.next());
+
+                    System.out.print("Etternavn: ");
+                    ny.setEtternavn(in.next());
+
+                    System.out.print("Brukernavn: ");
+                    ny.setBrukernavn(in.next());
+
+                    System.out.print("Stilling: ");
+                    ny.setStilling(in.next());
+
+                    System.out.print("Månedslønn: ");
+                    ny.setMaanedslonn(in.nextDouble());
+
+                    // resten kan settes senere (avdeling osv.)
+                    dao.lagreAnsatt(ny);
+
+                    System.out.println("Ny ansatt lagret.");
+                    break;
+
+                case 0:
+                    ferdig = true;
+                    break;
+
+                default:
+                    System.out.println("Ugyldig valg.");
+            }
         }
+
+        em.close();
+        in.close();
     }
 }
