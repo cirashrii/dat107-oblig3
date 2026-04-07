bf92a326da10a4ce7f7c794fdf439ba096c2a1f8
anrin2544 <ringstadandrea@gmail.com>
2026-04-07 14:32:40 +0200

    PK

---
+++ src/main/java/org/oblig3/ProsjektdeltagelsePK.java
@@ -0,0 +1,15 @@
+package org.oblig3;
+
+@SuppressWarnings("unused")
+public class ProsjektdeltagelsePK {
+
+    private int ansatt;   
+    private int prosjekt;
+
+    public ProsjektdeltagelsePK() {}
+
+    public ProsjektdeltagelsePK(int ansattId, int prosjektId) {
+        this.ansatt = ansattId;
+        this.prosjekt = prosjektId;
+    }
+}
